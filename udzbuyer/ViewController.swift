417f007a0e74cb39e91181a77a00664de59820ac
LiQiang <1977194097@qq.com>
2017-06-26 17:38:16 +0800

    添加项目

---
+++ udzbuyer/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  udzbuyer
+//
+//  Created by mayibailing1 on 2017/6/26.
+//  Copyright © 2017年 mayi121. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print(111);
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
